5b0e283b8c250bec5252f86a5916c1d3f7aaf838
Davide Filosa <davideclaypool@gmail.com!>
2022-11-21 11:29:28 +0100

    Implemented functionality to search and display image directly from the web

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
+import shutil
 
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
     def search_image(self):
-        self.manager.current_screen.ids.img.source = 'files/image.jpg'
+        search = self.manager.current_screen.ids.user_query.text
+        page = wikipedia.page(search)
+        link = page.images[0]
+        headers = {'User-Agent': 'copied user agent that came out when I googled it'}
+        image = requests.get(link, headers=headers)
+        path = f'files/{search}.jpg'
+        with open (path, 'wb') as img:
+            img.write(image.content)
+        self.manager.current_screen.ids.img.source = path
+
 
 
 class RootWidget(ScreenManager):
