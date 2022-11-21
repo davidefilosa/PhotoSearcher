a2179c54280c1ec5c32a5ec67cb5cd11b2e1acc3
Davide Filosa <davideclaypool@gmail.com!>
2022-11-21 11:57:34 +0100

    Final Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # What does this app do?
 
+Simple App created o ty for the first time Kivy https://kivy.org/.
+
+The app displays an image based on a search term entered by the user 
--- main.py
+++ main.py
@@ -3,23 +3,29 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 import wikipedia
 import requests
-import shutil
 
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
-    def search_image(self):
+    def get_image_link(self):
         search = self.manager.current_screen.ids.user_query.text
         page = wikipedia.page(search)
         link = page.images[0]
+        return link, search
+
+    def get_image(self):
+        link, search = self.get_image_link()
         headers = {'User-Agent': 'copied user agent that came out when I googled it'}
         image = requests.get(link, headers=headers)
         path = f'files/{search}.jpg'
+        return image, path
+
+    def save_image(self):
+        image, path = self.get_image()
         with open (path, 'wb') as img:
             img.write(image.content)
         self.manager.current_screen.ids.img.source = path
-
 
 
 class RootWidget(ScreenManager):
